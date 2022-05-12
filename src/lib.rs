b616ee09b58a084273d84c8ae477f495aa78029c
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-05-12 13:16:18 +0300

    naah

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,32 @@ use near_sdk::{
     assert_self,
     borsh::{self, BorshDeserialize, BorshSerialize},
     collections::LookupSet,
-    env,
+    env, ext_contract,
     json_types::U128,
-    near_bindgen, require, AccountId, Promise, ONE_NEAR,
+    near_bindgen, require, AccountId, Balance, Gas, Promise, ONE_NEAR,
 };
 
 use std::collections::HashMap;
-
-// 100Ⓝ in yoctoNEAR
-const MAX_WITHDRAW_AMOUNT: u128 = 100 * ONE_NEAR;
-// 30min in ms
+const TGAS: u64 = 1_000_000_000_000;
+const MAX_WITHDRAW_AMOUNT: Balance = 100 * ONE_NEAR;
 const REQUEST_GAP_LIMITER: u64 = 1800000;
+const VAULT_ID: &str = "vault.nonofficial.testnet";
+const MIN_BALANCE_THRESHOLD: Balance = 100 * ONE_NEAR;
+
+#[ext_contract(vault_contract)]
+trait VaultContract {
+    fn request_funds(&mut self);
+}
+
+#[ext_contract(faucet_contract)]
+trait Callbacks {
+    fn request_additional_liquidity(&self);
+}
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    top_contributors: Vec<(AccountId, u128)>,
+    top_contributors: Vec<(AccountId, Balance)>,
     recent_receivers: HashMap<AccountId, u64>,
     blacklist: LookupSet<AccountId>,
 }
@@ -34,7 +44,7 @@ impl Default for Contract {
 
 #[near_bindgen]
 impl Contract {
-    pub fn request_funds(&mut self, receiver_id: AccountId, amount: U128) -> Promise {
+    pub fn request_funds(&mut self, receiver_id: AccountId, amount: U128) {
         // check if predecessor is in the blacklist
         require!(
             self.blacklist.contains(&env::predecessor_account_id()) == false,
@@ -67,7 +77,14 @@ impl Contract {
             }
         }
         // make the transfer
-        Promise::new(receiver_id.clone()).transfer(amount.0)
+        Promise::new(receiver_id.clone()).transfer(amount.0).then(
+            faucet_contract::request_additional_liquidity(
+                env::current_account_id(),
+                0,
+                Gas::from(300000000000000),
+            ),
+        );
+        env::log_str("request fund finished");
     }
 
     // #[private] this macro does not expand for unit testing therefore I'm ignoring it for the time being
@@ -91,8 +108,8 @@ impl Contract {
     // contribute to the faucet contract to get in the list of fame
     #[payable]
     pub fn contribute(&mut self) {
-        let donator = env::predecessor_account_id();
-        let amount = env::attached_deposit();
+        let donator: AccountId = env::predecessor_account_id();
+        let amount: Balance = env::attached_deposit();
 
         match self
             .top_contributors
@@ -114,6 +131,14 @@ impl Contract {
             .iter()
             .map(|(account_id, amount)| (account_id.clone(), amount.to_string()))
             .collect()
+    }
+
+    #[private]
+    pub fn request_additional_liquidity(&self) {
+        env::log_str("Callback called");
+        if env::account_balance() < MIN_BALANCE_THRESHOLD {
+            vault_contract::request_funds(VAULT_ID.parse().unwrap(), 0, Gas(300_000_000_000_000));
+        }
     }
 }
 #[cfg(test)]
