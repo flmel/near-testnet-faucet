f45fb66e6cad1b2d6dee5ef9605ecf1d38934fd0
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-05-18 13:40:32 +0300

    contribute form, minor improvements

--- scripts.js
+++ scripts.js
@@ -14,6 +14,53 @@ const contract = new nearApi.Contract(wallet.account(), 'v1.faucet.nonofficial.t
     changeMethods: ['request_funds', 'contribute']
 })
 
+// AlpineJS store for global events/data
+document.addEventListener('alpine:init', () => {
+    const accountId = wallet.getAccountId()
+    Alpine.store('auth', {
+        sign_in() {
+            wallet.requestSignIn(
+                'v1.faucet.nonofficial.testnet',
+                'Testnet Faucet v1'
+            )
+        },
+        sign_out() {
+            wallet.signOut()
+            localStorage.removeItem(`near-api-js:keystore:${accountId}:testnet`)
+            accountId.value = accountId
+            this.signed = false
+        },
+
+        signed: wallet.isSignedIn()
+    })
+})
+
+// contribute form
+function contribute() {
+    async function get_balance() {
+        const response = await wallet.account().getAccountBalance()
+        return nearApi.utils.format.formatNearAmount(response.available).split(".")[0]
+    }
+
+    async function contributeToFaucet() {
+        await contract.contribute({}, "60000000000000",
+            nearApi.utils.format.parseNearAmount(this.contribute_amount).toString())
+    }
+
+    function validateInput() {
+        this.inputValid = this.contribute_amount !== "" && /^\d*$/.test(this.contribute_amount) && this.contribute_amount != 0
+    }
+
+    return {
+        get_balance,
+        contributeToFaucet,
+        validateInput,
+        inputValid: true,
+        open: false,
+        contribute_amount: 200
+    }
+}
+
 // contribution stats
 function contributionStats() {
     async function contributorsTopList() {
@@ -27,58 +74,35 @@ function contributionStats() {
     return { contributorsTopList }
 }
 
-// authentication
-function auth() {
-    const accountId = wallet.getAccountId()
-    function sign_in() {
-        wallet.requestSignIn(
-            'v1.faucet.nonofficial.testnet',
-            'Testnet Faucet v1'
-        )
-    }
-    function sign_out() {
-        wallet.signOut();
-        localStorage.removeItem(`near-api-js:keystore:${accountId}:testnet`)
-        accountId.value = wallet.getAccountId()
-        this.signed = false
-    }
-    return {
-        signed: wallet.isSignedIn(),
-        account_id: accountId,
-        sign_in,
-        sign_out
-    }
-}
-
 // faucet
 function faucet() {
     async function requestFunds() {
-        this.loading = true;
+        this.loading = true
         const receiver_id = this.receiver_id
         try {
             await contract.request_funds({
                 receiver_id,
                 amount: nearApi.utils.format.parseNearAmount((100).toString())
-            })
+            }, "50000000000000")
             this.success = {
                 status: true,
                 message: `Success! You have sent 100Ⓝ to ${receiver_id}!`
             }
-            this.loading = false;
+            this.loading = false
         } catch (err) {
             console.error(err)
             this.failure = {
                 status: true,
                 message: 'Sorry! You have to wait a little longer!'
             }
-            this.loading = false;
+            this.loading = false
         }
 
     }
     // clearNotifications
     function clearNotifications() {
-        this.success.status = false;
-        this.failure.status = false;
+        this.success.status = false
+        this.failure.status = false
     }
     // checkAccountExist
     async function checkAccountExist() {
